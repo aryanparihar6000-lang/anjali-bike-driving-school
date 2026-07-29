54b0f3c4cf59ade5a9e312a6b4e75473dfeb8a22
aryanparihar6000-lang <aryanparihar6000@gmail.com>
2026-07-29 20:23:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -567,6 +567,12 @@ Training Duration: ${duration}
 Certificate ID: ${certificateID}
 </p>
 
+<img 
+src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${certificateID}"
+style="
+width:120px;
+margin-top:20px;
+">
 
 <p>
 Date: ${date}
